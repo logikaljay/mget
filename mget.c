0d99b1f744a56abec5e0fb670b4fdb689032aef5
jay <jay@localcentos.(none)>
2013-10-29 13:46:56 +1300

    * Include math library in build script * got some output from each of the curl handles that download the files

--- mget.c
+++ mget.c
@@ -1,14 +1,85 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <curl/curl.h>
+
+double toDownload = 0;
+double totalDownloaded;
+int handles = 0;
+
+typedef struct progress {
+	int handle;
+	double totalDownloaded;
+	double segmentSize;
+} *dl_progress;
+
+struct progress MyProgress[];
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) 
+{
+	size_t written;
+	written = fwrite(ptr, size, nmemb, stream);
+	return written;
+}
 
 static size_t get_size_struct(void *ptr, size_t size, size_t nmemb, void *data)
 {
-  (void)ptr;
-  (void)data;
-  /* we are not interested in the headers itself,
-     so we only return the size we would have saved ... */ 
-  return (size_t)(size * nmemb);
+	(void)ptr;
+	(void)data;
+
+	// return only the size, dump the rest
+	return (size_t)(size * nmemb);
+}
+
+int build_progress_bar()
+{
+	int totalDots = 40;
+	double fractionDownloaded = totalDownloaded / toDownload;
+	int dots = round(fractionDownloaded * totalDots);
+	
+	// create the meter
+	int ii = 0;
+	printf("%3.0f%% [", fractionDownloaded*100);
+	
+	// part thats downloaded
+	for ( ; ii < dots; ii++) {
+		printf("=");
+	}
+	
+	for ( ; ii < totalDots; ii++) {
+		printf(" ");
+	}
+	
+	printf("]\r");
+	fflush(stdout);
+	
+	return 0;
+}
+
+int display_progress()
+{
+	int i = 0;
+	for ( ; i < handles; i++) {
+		struct progress pgr = MyProgress[i];
+		int totaldots = 40;
+		double totalDownloaded = pgr.totalDownloaded;
+		double segmentSize = pgr.segmentSize;
+		double fractionDownloaded = totalDownloaded / segmentSize;
+		printf("handle %d: %0.0f / %0.0f\n", i, totalDownloaded, segmentSize);
+	}
+	printf("\n");
+	
+	return 0;
+}
+
+int progress_func(dl_progress ptr, double totalToDownload, double nowDownloaded, double totalToUpload, double nowUploaded)
+{
+	int currentHandle = ptr->handle;
+	ptr->totalDownloaded = nowDownloaded;
+	
+	//printf("pdata->handle: %d = %0.0f/%0.0f\n", ptr->handle, ptr->totalDownloaded, ptr->segmentSize);
+	
+	return display_progress();
 }
 
 static double get_download_size(char** url)
@@ -22,7 +93,6 @@ static double get_download_size(char** url)
 	curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
 	curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, get_size_struct);
 	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-	//curl_easy_setopt(curl, CURLOPT_HEADER, 0L);
 	res = curl_easy_perform(curl);
 	res = curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &size);
 	if (res!=CURLE_OK) {
@@ -32,12 +102,6 @@ static double get_download_size(char** url)
 	curl_easy_cleanup(curl);
 	
 	return size;
-}
-
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-	size_t written;
-	written = fwrite(ptr, size, nmemb, stream);
-	return written;
 }
 
 main(int argc, char **argv[])
@@ -54,6 +118,7 @@ main(int argc, char **argv[])
 	double partSize = 0;
 	double segLocation = 0;
 	int still_running;
+	handles = parts;
 	int i; // iterator
 
 	// get file name
@@ -61,6 +126,7 @@ main(int argc, char **argv[])
 	
 	// get file size
 	double size = get_download_size(argv[2]);
+	toDownload = size;
 	partSize = size / parts;
 
 	// output some file size/segement size info
@@ -78,6 +144,11 @@ main(int argc, char **argv[])
 	curl_global_init(CURL_GLOBAL_ALL);
 
 	for (i=0; i<parts; i++) {
+	
+		MyProgress[i].handle = i;
+		MyProgress[i].segmentSize = partSize;
+		MyProgress[i].totalDownloaded = 0;
+		
 		// setup our output filename
 		char filename[50];
 		sprintf(filename, "%s.part.%0d", outputfile, i);
@@ -85,6 +156,7 @@ main(int argc, char **argv[])
 		// allocate curl handle for each segment
 		handles[i] = curl_easy_init();
 		fileparts[i] = fopen(filename, "w");
+		totalDownloaded = 0;
 		
 		double nextPart = 0;
 		if (i == parts - 1) {
@@ -100,6 +172,9 @@ main(int argc, char **argv[])
 		curl_easy_setopt(handles[i], CURLOPT_URL, url);
 		curl_easy_setopt(handles[i], CURLOPT_RANGE, range);
 		curl_easy_setopt(handles[i], CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(handles[i], CURLOPT_NOPROGRESS, 0);
+		curl_easy_setopt(handles[i], CURLOPT_PROGRESSFUNCTION, progress_func);
+		curl_easy_setopt(handles[i], CURLOPT_PROGRESSDATA, &MyProgress[i]);
 		curl_easy_setopt(handles[i], CURLOPT_WRITEDATA, fileparts[i]);
 
 		segLocation = segLocation + partSize;
